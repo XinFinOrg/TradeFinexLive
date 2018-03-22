fc9dc03083c7c0c70b5fbd6307593f8fd2e777c7
unknown <sahasouvik9@gmail.com>
2018-03-22 15:33:46 +0530

    API SEARCH UPDATED FOOTER

--- application/views/includes/footern.php
+++ application/views/includes/footern.php
@@ -8,7 +8,7 @@
 		<div class="button-go-top">
 			<a href="javascript:void(0)" title="" class="go-top"> <i class="fa fa-chevron-up"></i> </a><!-- /.go-top --> 
 		</div>
-		
+		<div id="fadew" class="white_overlay"></div>
 		<div id="fade" class="black_overlay"></div>
 	</body>
 </html>
