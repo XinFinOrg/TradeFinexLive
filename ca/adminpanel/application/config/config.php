8c2793e78035f009a302588a8073505434507803
mansivora <mansivora36@gmail.com>
2018-07-06 10:35:51 +0530

    Changes

--- ca/adminpanel/application/config/config.php
+++ ca/adminpanel/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://locahost/trade/adminpanel/';
+$config['base_url'] = 'https://ca.tradefinex.org/adminpanel';
 
 /*
 |--------------------------------------------------------------------------
