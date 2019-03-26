2bbac9311be255f705a734daf641091e50d8935d
mansivora <mansivora36@gamail.com>
2019-03-26 19:12:44 +0530

    common scripts changed

--- assets/js/page_js/common_scripts.js
+++ assets/js/page_js/common_scripts.js
@@ -1157,8 +1157,8 @@ $(function () {
 	$('#createBondHeader').click(function() {
 		//console.log('alert')
 
-		$('#bond_create-form').find("input[type=text], textarea").val("");
-
+		// $('#bond_create-form').find("input[type=text], textarea").val("");
+		location.reload();
 	});
 	
 	$('#sorry').click(function() {
