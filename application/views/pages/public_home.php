23138aa25543e6352ed34894554fbd6ed2f5c2c6
mansivora <mansivora36@gamail.com>
2019-07-04 12:50:23 +0530

    chatbot added

--- application/views/pages/public_home.php
+++ application/views/pages/public_home.php
@@ -583,3 +583,16 @@
 </div> -->
 
 <?php $this->load->view('includes/login_modal'); ?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5d1d98c77a48df6da242e25b/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
