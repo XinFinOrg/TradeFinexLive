41fb89ccaa4a72c9a30acc9f677364423cb8e75b
mansivora <mansivora36@gmail.com>
2020-02-06 16:03:26 +0530

    Site title changed

--- application/views/includes/headern.php
+++ application/views/includes/headern.php
@@ -6,14 +6,37 @@
 <!--<marquee>The website is under Maintenence. Please ignore if you face any technical issue.</marquee>-->
 <head>
     <meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="TradeFinex" content="TradeFinex is a decentralized peer to peer global Trade & Infrastructure Finance platform built on open standards and it provides seamless connectivity between global public investors, contract authority, project company, regulators and other key project finance stakeholders" />
-	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
-	
-	<!-- Mobile Specific Metas -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<title>TradeFinex</title>
+	<title>TradeFinex | Trade Finance Distribution powered by XinFin [XDC] Blockchain Network</title>
 	
-	<!-- Favicon -->
-	<link rel="icon" href="<?php echo base_url() ?>assets/images/icon/favicon.png" />
+	<!-- Favicon -->	
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url() ?>assets/images/icon/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="<?php echo base_url() ?>assets/images/icon/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url() ?>assets/images/icon/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url() ?>assets/images/icon/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url() ?>assets/images/icon/favicon/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo base_url() ?>assets/images/icon/favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="<?php echo base_url() ?>assets/images/icon/favicon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 	
+	<meta name="description" content="A Blockchain Network for Trade Finance Originators to Distribute Trade Assets to the Growing Class of Alternative Asset Investors." />
+	<meta name="keywords" content="Originate to distribute, Originate to distribute network, Originate to distribute platform, Blockchain Network for Trade Finance Originators, Distribute tarde assets, TradeFinex">
+	<link rel="canonical" href="https://www.tradefinex.org/" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="TradeFinex | Trade Finance Distribution powered by XinFin [XDC] Blockchain Network" />
+	<meta property="og:description" content="A Blockchain Network for Trade Finance Originators to Distribute Trade Assets to the Growing Class of Alternative Asset Investors." />
+	<meta property="og:url" content="https://www.tradefinex.org/" />
+	<meta property="og:site_name" content="TradeFinex" />
+
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
