1283c09aff38b0b000fd36a9ec3f5a8ed1ff1e4f
mansivora <>
2020-02-20 18:51:27 +0530

    statistics script changed

--- application/views/pages/public/statistics_view.php
+++ application/views/pages/public/statistics_view.php
@@ -507,8 +507,16 @@
         });	
 		
 		
-		function numberWithCommas(x) {
-    		return x.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
+		function numberWithCommas(nStr){
+			nStr += '';
+			var x = nStr.split('.');
+			var x1 = x[0];
+			var x2 = x.length > 1 ? '.' + x[1] : '';
+			var rgx = /(\d+)(\d{3})/;
+			while (rgx.test(x1)) {
+			x1 = x1.replace(rgx, '$1' + ',' + '$2');
+			}
+			return x1 + x2;
 		}
 		$('.counter').each(function(){
 			var v_pound = $(this).html();
