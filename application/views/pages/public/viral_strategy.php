04203c28c7118d6ae33beebcab8ad7d3f738a8fa
mansivora <mansivora36@gmail.com>
2020-03-19 10:17:27 +0530

    strategy page added

---
+++ application/views/pages/public/viral_strategy.php
@@ -0,0 +1,83 @@
+<!-- Inside Page -->
+<div class="sub_page_wraper">
+
+    <section class="tf-inner-banner">
+        <div class="container">
+            <h3>Is Your business affected by coronavirus or external crisis ?</h3>
+            <h4>TradeFinex lets you connect with funding sources that provide easy credit facilities.</h4>
+        </div>
+    </section>
+
+    <!-- About XinFin Masternode -->
+    <section id="tf-setup-masternode" class="section">
+        <div class="container">
+            <div class="row">
+                <div class="section-title text-center">
+                    <h2 class="mb-0">How to start?</h2>
+                </div>
+                <div class="col-md-8 col-md-offset-2">
+                </div>
+            </div>
+
+            <div class="row flex-row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-30">
+                    <div class="strategy-howTo-card">
+                        <div class="strategy-howTo-card-head media">
+                            <div class="ico-step">
+                                <p>STEP <span>1</span></p>
+                            </div>
+                            <h4>Start Business Registration</h4>
+                        </div>
+                        <p>Start <a href="<?php echo base_url();?>/publicv/buyersupplier" target="_blank">Business Registration</a> and create identity on TradeFinex (need Business Registration document).</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-30">
+                    <div class="strategy-howTo-card">
+                        <div class="strategy-howTo-card-head media">
+                            <div class="ico-step">
+                                <p>STEP <span>2</span></p>
+                            </div>
+                            <h4>Start Request for the Funding</h4>
+                        </div>
+                        <p>Businesses can share this certificate over LinkedIn or other social medias, tag major local Bank and Non-Bank funders.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-30">
+                    <div class="strategy-howTo-card">
+                        <div class="strategy-howTo-card-head media">
+                            <div class="ico-step">
+                                <p>STEP <span>3</span></p>
+                            </div>
+                            <h4>Start Communication for the Funding</h4>
+                        </div>
+                        <p>Funder sees the document and starts communication for the funding.</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <!--<div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                        <p>Step 1: Start <strong><a href="<?php echo base_url();?>/publicv/buyersupplier" target="_blank">Business Registration</a> </strong>and create identity on TradeFinex (need Business Registration document)</p>
+                        <p> Step 2: Start Request for the funding:</li>
+                        <div class="bulletList">
+                            <ul>
+                                <li>Businesses can share this certificate over LinkedIn or other social medias, tag major local Bank and Non-Bank funders.</li>
+                            </ul>
+                        </div>
+                        <p> Step 3: Funder sees the document and starts communication for the funding.</p> 
+                    </ol>
+                </div>
+            </div>-->
+        </div>
+    </section>
+    <!-- /. About XinFin Masternode -->
+
+</div>
+<!-- /. Inside Page About XinFin Masternode -->
+
+<?php
+	//$this->load->view('includes/block_features');	
+	$this->load->view('includes/login_modal');
+
+?>
